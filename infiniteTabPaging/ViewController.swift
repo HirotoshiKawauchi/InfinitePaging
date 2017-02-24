fb094e9d0a8fb6f3b274edd4b32a4a2d37149182
hirotoshi <hirotoshi_note_pc@yahoo.co.jp>
2017-02-24 22:41:19 +0900

    Initial Commit

---
+++ infiniteTabPaging/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  infiniteTabPaging
+//
+//  Created by HIROTOSHI KAWAUCHI on 2017/02/24.
+//  Copyright © 2017年 HIROTOSHI KAWAUCHI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
