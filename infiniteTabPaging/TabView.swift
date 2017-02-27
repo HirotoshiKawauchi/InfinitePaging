99b3524e7504701f2a02852e4f1447a868a48bee
hirotoshi <hirotoshi_note_pc@yahoo.co.jp>
2017-02-28 01:46:01 +0900

    reUpdate

---
+++ infiniteTabPaging/TabView.swift
@@ -0,0 +1,16 @@
+//
+//  TabView.swift
+//  infiniteTabPaging
+//
+//  Created by HIROTOSHI KAWAUCHI on 2017/02/24.
+//  Copyright © 2017年 HIROTOSHI KAWAUCHI. All rights reserved.
+//
+
+import UIKit
+
+class TabView: UIView {
+
+    @IBOutlet weak var leadingMargin: NSLayoutConstraint!
+    @IBOutlet weak var trailingMargin: NSLayoutConstraint!
+
+}
